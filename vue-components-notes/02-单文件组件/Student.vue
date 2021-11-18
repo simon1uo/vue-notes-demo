070584bb6898f4531057f35f24f817332d0ba82a
Simon Law <simonlaw910@gmail.com>
2021-11-18 18:31:13 +0800

    ⏫ 更新Vue组件化编程内容

---
+++ vue-components-notes/02-单文件组件/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>学生</h1>
+    <h2>name: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student", // 组件名
+  data(){
+    return{
+      name: 'Simon',
+      age: 21
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-components-notes/02-单文件组件/main.js
@@ -0,0 +1,11 @@
+// main.js vm实例
+// 入口文件
+import App from "./App";
+
+new Vue({
+    el: '#root',
+    template: `<App></App>`,
+    components: {
+        App
+    }
+})
