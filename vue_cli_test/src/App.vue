a6157cb70ab1b57f5c18ce0be1d80c9db59c28b7
Simon Law <simonlaw910@gmail.com>
2021-11-18 21:35:57 +0800

    ⏫更新脚手架学习示例

--- vue_cli_test/src/App.vue
+++ vue_cli_test/src/App.vue
@@ -1,28 +1,23 @@
+<!--App组件总汇所有组件-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img src="./assets/logo.png" alt="">
+    <School></School>
+    <hr>
+    <Student></Student>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 引入组件
+import School from "./components/School";
+import Student from "./components/Student";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components:{
+    School,
+    Student
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue_cli_test/src/main.js
+++ vue_cli_test/src/main.js
@@ -1,8 +1,19 @@
+// 项目入口文件
+
+
+// 引入Vue库
 import Vue from 'vue'
+// 引入App组件，所有组件的符组件
 import App from './App.vue'
 
+// 关闭Vue生产提示
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    // el: "#app", 指定容器
+    render: h => h(App),
+
+    /* render(createElement){
+      return createElement('h1', 'TestRender')
+    }*/
 }).$mount('#app')
