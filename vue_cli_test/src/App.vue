5180ed883e840283bc121e663ec47a3bf9d4899c
Simon Law <simonlaw910@gmail.com>
2021-11-20 10:39:40 +0800

    ⏫ 更新Vue脚手架实例项目和代码示例

--- vue_cli_test/src/App.vue
+++ vue_cli_test/src/App.vue
@@ -1,13 +1,12 @@
 <!--App组件总汇所有组件-->
 <template>
   <div>
-    <Student/>
+    <Student name="Simon" :age="20"/>
   </div>
 </template>
 
 <script>
 // 引入组件
-// import School from "./components/School";
 import Student from "./components/Student";
 
 export default {
--- vue_cli_test/src/components/Student.vue
+++ vue_cli_test/src/components/Student.vue
@@ -2,19 +2,18 @@
   <div>
     <h1>学生信息</h1>
     <h2>name: {{ name }}</h2>
-    <h2>age: {{ age }}</h2>
+    <h2>age: {{ age + 1 }}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: "Student", // 组件名
-  data(){
-    return{
-      name: 'Simon',
-      age: 21
+  data() {
+    return {
+      msg: 'Hello Student'
     }
-  }
+  }, props: ['name', 'age']
 }
 </script>
 
